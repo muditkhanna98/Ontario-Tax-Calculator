9957bd8f9deaf01b57a7493488b29ef6a557858d
Mudit Khanna <muditkhanna998@gmail.com>
2023-04-14 18:58:41 -0400

    adds shared preferences and tax logics

--- app/src/main/java/com/mudit/taxcalculator/MainActivity.java
+++ app/src/main/java/com/mudit/taxcalculator/MainActivity.java
@@ -2,13 +2,110 @@ package com.mudit.taxcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.google.android.material.slider.Slider;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView headingTextView;
+    private EditText incomeEditText;
+    private TextView rrspTextView;
+    private SeekBar rrspSeekBar;
+    private TextView rrsp_multiplierText;
+    private TextView ontarioTaxTextView;
+    private TextView federalTaxTextView;
+    private TextView surtaxTextView;
+    private TextView rrspContributionTv;
+
+    private SharedPreferences sharedPreferences;
+    private double rrspMultiplier;
+    private double rrspDeduction;
+    private TaxCalculator taxCalculator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        headingTextView = findViewById(R.id.headingTextView);
+        incomeEditText = findViewById(R.id.incomeEditText);
+        rrspTextView = findViewById(R.id.rrspTextView);
+        rrspSeekBar = findViewById(R.id.rrsp_seekbar);
+        rrsp_multiplierText = findViewById(R.id.rrsp_multiplierText);
+        rrspContributionTv = findViewById(R.id.rrspContributionTv);
+        ontarioTaxTextView = findViewById(R.id.ontarioTaxTextView);
+        federalTaxTextView = findViewById(R.id.federalTaxTextView);
+        surtaxTextView = findViewById(R.id.surtaxTextView);
+
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        taxCalculator = new TaxCalculator();
+
+        rrspSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                // Calculate the RRSP deduction based on the SeekBar progress
+                rrspMultiplier = (double) progress / 1000.0;
+                rrspDeduction = rrspMultiplier * 27230;
+                updateTaxOutput(rrspDeduction, rrspMultiplier);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // Do nothing
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // Do nothing
+            }
+        });
+        fetchSharedPreferences();
+    }
+
+    public void updateTaxOutput(double rrspDeduction, double rrspMultiplier) {
+        Double income = Double.parseDouble(incomeEditText.getText().toString());
+        Double ontarioTax = taxCalculator.calculateOntarioTax(income, rrspDeduction);
+        Double federalTax = taxCalculator.calculateFederalTax(income, rrspDeduction);
+        Double surtax = taxCalculator.calculateSurtax(income, rrspDeduction, ontarioTax);
+
+        rrsp_multiplierText.setText("RRSP Multiplier: " + String.format("%.2f", rrspMultiplier));
+        ontarioTaxTextView.setText("Ontario tax: " + String.format("%.2f", ontarioTax));
+        federalTaxTextView.setText("Federal tax: " + String.format("%.2f", federalTax));
+        surtaxTextView.setText("Surtax: " + String.format("%.2f", surtax));
+        rrspContributionTv.setText("RRSP Contribution: " + String.format("%.2f", rrspDeduction));
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("income", String.format("%.2f", income));
+        editor.putString("multiplier", "RRSP Multiplier: " + String.format("%.2f", rrspMultiplier));
+        editor.putString("ontarioTax", "Ontario tax: " + String.format("%.2f", ontarioTax));
+        editor.putString("federalTax", "Federal tax: " + String.format("%.2f", federalTax));
+        editor.putString("surtax", "Surtax: " + String.format("%.2f", surtax));
+        editor.putString("rrsDeduction", "RRSP Contribution: " + String.format("%.2f", rrspDeduction));
+        editor.apply();
+    }
+
+    public void fetchSharedPreferences() {
+        String income = sharedPreferences.getString("income", "0.0");
+        String multiplier = sharedPreferences.getString("multiplier", "RRSP Multiplier: ");
+        String ontarioTax = sharedPreferences.getString("ontarioTax", "Ontario tax: ");
+        String federalTax = sharedPreferences.getString("federalTax", "Federal Tax: ");
+        String surtax = sharedPreferences.getString("surtax", "Surtax: ");
+        String rrspDeduction = sharedPreferences.getString("rrspDeduction", "RRSP Contribution: ");
+
+        rrsp_multiplierText.setText(multiplier);
+        incomeEditText.setText(income);
+        ontarioTaxTextView.setText(ontarioTax);
+        federalTaxTextView.setText(federalTax);
+        surtaxTextView.setText(surtax);
+        rrspContributionTv.setText(rrspDeduction);
+
+        String[] multiplierArr = multiplier.split(":");
+        int progValue = (int) (Double.parseDouble(multiplierArr[1].trim()) * 1000);
+        rrspSeekBar.setProgress(progValue, true);
     }
 }
--- app/src/main/java/com/mudit/taxcalculator/TaxCalculator.java
+++ app/src/main/java/com/mudit/taxcalculator/TaxCalculator.java
@@ -1,37 +1,18 @@
 package com.mudit.taxcalculator;
 
 public class TaxCalculator {
-    private double[] ontarioBrackets = {0, 48430, 96860, 150000, 220000};
-    private double[] ontarioRates = {0.0505, 0.0915, 0.1116, 0.1216, 0.1316};
-    private double[] federalBrackets = {0, 49020, 98040, 151978, 216511};
-    private double[] federalRates = {0.15, 0.205, 0.26, 0.29, 0.33};
-    private double[] surtaxRates = {0.20, 0.36, 0.41, 0.44};
-    private static final double RRSP_LIMIT = 27570;
+    private final double[] ontarioBrackets = {0, 48430, 96860, 150000, 220000};
+    private final double[] ontarioRates = {0.0505, 0.0915, 0.1116, 0.1216, 0.1316};
+    private final double[] federalBrackets = {0, 49020, 98040, 151978, 216511};
+    private final double[] federalRates = {0.15, 0.205, 0.26, 0.29, 0.33};
+    private final double[] surtaxRates = {0.20, 0.36, 0.48};
 
-    public double calculateTax(double income, double rrsp) {
-        // Deduct RRSP contribution from income
-        double taxableIncome = income - Math.min(rrsp, RRSP_LIMIT);
-
-        // Calculate federal tax
-        double federalTax = calculateFederalTax(taxableIncome);
-
-        // Calculate Ontario tax
-        double ontarioTax = calculateOntarioTax(taxableIncome);
-
-        // Calculate surtax if applicable
-        double surtax = calculateSurtax(income);
-
-        // Calculate total tax
-        double totalTax = federalTax + ontarioTax + surtax;
-
-        return totalTax;
-    }
-
-    private double calculateFederalTax(double income) {
+    public double calculateFederalTax(double income, double rrsp) {
+        double taxableIncome = income - rrsp;
         double federalTax = 0;
         for (int i = 0; i < federalBrackets.length; i++) {
-            if (income > federalBrackets[i]) {
-                double incomeInBracket = Math.min(income - federalBrackets[i], federalBrackets[i + 1] - federalBrackets[i]);
+            if (taxableIncome > federalBrackets[i]) {
+                double incomeInBracket = Math.min(taxableIncome - federalBrackets[i], federalBrackets[i + 1] - federalBrackets[i]);
                 federalTax += incomeInBracket * federalRates[i];
             } else {
                 break;
@@ -40,11 +21,12 @@ public class TaxCalculator {
         return federalTax;
     }
 
-    private double calculateOntarioTax(double income) {
-        double ontarioTax = 0;
+    public double calculateOntarioTax(double income, double rrsp) {
+        double taxableIncome = income - rrsp;
+        double ontarioTax = 0.0;
         for (int i = 0; i < ontarioBrackets.length; i++) {
-            if (income > ontarioBrackets[i]) {
-                double incomeInBracket = Math.min(income - ontarioBrackets[i], ontarioBrackets[i + 1] - ontarioBrackets[i]);
+            if (taxableIncome > ontarioBrackets[i]) {
+                double incomeInBracket = Math.min(taxableIncome - ontarioBrackets[i], ontarioBrackets[i + 1] - ontarioBrackets[i]);
                 ontarioTax += incomeInBracket * ontarioRates[i];
             } else {
                 break;
@@ -53,27 +35,23 @@ public class TaxCalculator {
         return ontarioTax;
     }
 
-    private double calculateSurtax(double income) {
-        double surtax = 0;
-        double baseTax = calculateOntarioTax(income);
-
-        if (baseTax > 0) {
+    public double calculateSurtax(double income, double rrsp, double ontarioTax) {
+        double taxableIncome = income - rrsp;
+        double surtax = 0.0;
+        double surtaxThreshold = 484930;
+        if (taxableIncome > surtaxThreshold) {
+            double surtaxRate = 0.0;
             for (int i = 0; i < surtaxRates.length; i++) {
-                double surtaxIncome = baseTax * surtaxRates[i];
-                if (income > surtaxIncome) {
-                    if (i == 0) {
-                        surtax += surtaxIncome * (surtaxRates[i]);
-                    } else {
-                        surtax += surtaxIncome * (surtaxRates[i] - surtaxRates[i - 1]);
-                    }
-                } else {
-                    surtax += (income - baseTax) * (surtaxRates[i] - surtaxRates[i - 1]);
+                surtaxRate = surtaxRates[i];
+                if (ontarioTax <= surtaxThreshold * surtaxRate) {
                     break;
                 }
             }
+            surtax = (ontarioTax - surtaxThreshold * surtaxRate) * surtaxRate;
         }
         return surtax;
     }
+
 }
 
 
